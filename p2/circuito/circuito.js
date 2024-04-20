c6700dcd2d4cb30d8a8def92e91026a1d2b4b68a
Mario <lopezquesadamario@gmail.com>
2024-04-20 21:07:28 +0200

    circuito v1.0

---
+++ p2/circuito/circuito.js
@@ -0,0 +1,82 @@
+
+import * as THREE from '../libs/three.module.js'
+import { CSG } from '../libs/CSG-v2.js'
+
+
+
+
+class circuito extends THREE.Object3D {
+  constructor() {
+    
+    super();
+
+    const torusKnotGeom = new THREE.TorusKnotGeometry(9,1,300,12,6,8,);
+    const torusKnotMat = new THREE.MeshBasicMaterial( { color: 'lightblue' } ); 
+    const torusKnot = new THREE.Mesh( torusKnotGeom, torusKnotMat ); 
+    
+    //this.add( torusKnot );
+
+    const spline = this.getPathFromTorusKnot(torusKnot);
+
+    // Creamos un tubo a partir de la spline
+    const geometry = new THREE.TubeGeometry(spline, 300, 0.5, 8, false);
+    const material = new THREE.MeshBasicMaterial({ color: 'blue' , side: THREE.DoubleSide });
+    const tube = new THREE.Mesh(geometry, material);
+
+    this.add(tube);
+    // Añade edges al tubo
+    const edges = new THREE.EdgesGeometry(geometry);
+    const edgesMaterial = new THREE.LineBasicMaterial({ color: 'black' });
+    const edgesMesh = new THREE.LineSegments(edges, edgesMaterial);
+    this.add(edgesMesh);
+
+/*
+    var points = spline.getPoints(100);
+    var geometry = new THREE.BufferGeometry().setFromPoints(points);
+    var material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+    var curveObject = new THREE.Line(geometry, material);
+    //this.add(curveObject);
+
+    var geometry = new THREE.TubeGeometry(spline, 100, 0.1, 8, false);
+    var material = new THREE.MeshBasicMaterial({ color: 'lightpink' , side: THREE.DoubleSide });
+    var tube = new THREE.Mesh(geometry, material);
+*/
+    //this.add(tube);
+    
+  }
+
+  update() {
+      this.rotation.y += 0.01;
+  }
+
+  getPathFromTorusKnot (torusKnot) {
+    // La codificación de este método está basado
+    //   en el código fuente de la página oficial de Three.js
+    // https://github.com/mrdoob/three.js/blob/master/src/geometries/TorusKnotGeometry.js
+    const p = torusKnot.geometry.parameters.p;
+    const q = torusKnot.geometry.parameters.q;
+    const radius = torusKnot.geometry.parameters.radius;
+    const resolution = torusKnot.geometry.parameters.tubularSegments;
+    var u, cu, su, quOverP, cs;
+    var x,y,z;
+    // En  points  se almacenan los puntos que extraemos del torusKnot
+    const points = [];
+    for ( let i = 0; i < resolution; ++ i ) {
+      u = i / resolution * p * Math.PI * 2;
+      cu = Math.cos( u );
+      su = Math.sin( u );
+      quOverP = q / p * u;
+      cs = Math.cos( quOverP );
+  
+      x = radius * ( 2 + cs ) * 0.5 * cu;
+      y = radius * ( 2 + cs ) * su * 0.5;
+      z = radius * Math.sin( quOverP ) * 0.5;
+  
+      points.push (new THREE.Vector3 (x,y,z));
+    }
+    // Una vez tenemos el array de puntos 3D construimos y devolvemos el CatmullRomCurve3
+    return new THREE.CatmullRomCurve3 (points, true);
+  }
+}
+
+export { circuito };
